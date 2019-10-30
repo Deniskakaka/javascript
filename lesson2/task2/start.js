400813717b468a2e2c297009be805490949f3652
Denis <krollik1000000@gmail.com>
2019-10-30 21:20:30 +0200

    js

--- lesson2/task2/start.js
+++ lesson2/task2/start.js
@@ -14,5 +14,4 @@ const m = function run() {
     return 17;
 };
 
-console.log('a: ', typeof a, 'b: ', typeof b, 'c: ',
-    typeof c, 'd: ', typeof d, 'e: ', typeof e, 'f: ', typeof f, 'g: ', typeof g, 'h: ', typeof h, 'i: ', typeof i, 'j: ', typeof j, 'k: ', typeof k, 'l: ', typeof l, 'm: ', typeof m);
+console.log('a: ', typeof a);
