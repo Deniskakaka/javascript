1376ffbf0734a53b9effa45f7fea208282048d70
Denis <krollik1000000@gmail.com>
2019-10-30 21:00:51 +0200

    js

--- lesson2/task1/index.js
+++ lesson2/task1/index.js
@@ -3,6 +3,6 @@ const greeting = "I'm John";
 const profession = 'Developer';
 const $hobby = 'Walking';
 const favouriteDESSERT = 'cake';
-let pet_ = 'dog';
+let pet = 'dog';
 let superNumber;
 superNumber = 17;
