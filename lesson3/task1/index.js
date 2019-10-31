4edd36639a1a20896c5dc90ac5ee6bc23f10366a
Denis <krollik1000000@gmail.com>
2019-10-31 12:24:10 +0200

    js

--- lesson3/task1/index.js
+++ lesson3/task1/index.js
@@ -25,9 +25,9 @@ console.log('5 - undefined: ', 5 - undefined);
 console.log('"text" + null: ', 'text + null');
 // Постфиксная и префиксная форма записи
 ++num;
-console.log('++num: ', num);
+console.log('++num : ', num);
 num++;
-console.log('num++: ', num);
+console.log('num++ : ', num++);
 
 // математические операторы приводят операнды к числам
 const a = 17;
