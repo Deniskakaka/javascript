45f112f3261c7eecdb779ed987b416956c304675
Denis <krollik1000000@gmail.com>
2019-10-31 17:00:51 +0200

    js

--- lesson4/task5/index.js
+++ lesson4/task5/index.js
@@ -2,17 +2,14 @@ const m = 1;
 const n = 50;
 let result = 0;
 
-for (let i = 0; i <= n; i++) {
-    if (i % 2 == 0 && i % 4 != 0) {
+for (let i = m; i <= n; i++) {
+    if (!(i % 2) && (i % 4)) {
         result += i;
-    }
-    if (i % 3 == 0) {
+    } else if (!(i % 3)) {
         result -= i;
-    }
-    if (i % 4 == 0) {
+    } else if (!(i % 4)) {
         result *= i;
-    }
-    if (i % 5 == 0) {
+    } else if (!(i % 5)) {
         console.log(i);
     }
 }
