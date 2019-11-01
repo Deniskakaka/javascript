141a2d4dc1e3ab651499989286650b12e88281ff
Denis <krollik1000000@gmail.com>
2019-11-01 14:41:26 +0200

    js

--- lesson5/task6/index.js
+++ lesson5/task6/index.js
@@ -1,11 +1,15 @@
 function getPrimes(num) {
     for (let i = 2; i <= num; i++) {
-        for (let j = 1; j < i; j++) {
-            if (i % j == 0) {
-                console.log(i)
+        let count = 0;
+        for (let j = 1; j <= i; j++) {
+            if (i % j) {
+                continue;
             }
+            count++;
+        }
+        if (count == 2) {
+            console.log(i)
         }
     }
 }
-
 getPrimes(10)
