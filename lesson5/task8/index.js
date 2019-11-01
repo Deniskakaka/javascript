a22be0be0aa66d196fd50ee5d57bbda1b1ef8617
Denis <krollik1000000@gmail.com>
2019-11-01 15:09:53 +0200

    js

--- lesson5/task8/index.js
+++ lesson5/task8/index.js
@@ -2,12 +2,12 @@ function compareSums(a, b, c, d) {
     return sum(a, b, c, d)
 };
 
-function sum() {
+function sum(from, to) {
     from = arguments[0] + arguments[1];
     to = arguments[2] + arguments[3];
     if (from > to) {
-        return true;
+        console.log(true);
     } else {
-        return false;
+        console.log(false);
     }
 }
