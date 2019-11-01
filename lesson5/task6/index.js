1b489a2984a6861d9fabfb75afd9d40a3da1836e
Denis <krollik1000000@gmail.com>
2019-11-01 18:09:29 +0200

    js

--- lesson5/task6/index.js
+++ lesson5/task6/index.js
@@ -1,5 +1,5 @@
 function getPrimes(num) {
-    for (var i = 2; i < num; i++) {}
-    if (num % i === 0) return false;
+    for (var i = 2; i < num; i++)
+        if (num % i === 0) return false;
     return num > 1;
 }
