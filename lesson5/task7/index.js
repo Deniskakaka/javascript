6b98310411d805b9b12aa62b291b9a5685fbc163
Denis <krollik1000000@gmail.com>
2019-11-01 14:54:40 +0200

    js

--- lesson5/task7/index.js
+++ lesson5/task7/index.js
@@ -1,10 +1,11 @@
 function increaser(a, index) {
     if (index < a) {
-        return a;
+        return a * index;
     }
     if (!isInteger(index) || !isInteger(a)) {
         return false;
-    } else {
-        return a * index;
+    }
+    if (index > a) {
+        return a;
     }
 }
