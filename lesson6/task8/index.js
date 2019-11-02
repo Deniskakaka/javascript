e22717b97ac57a6e7343a5a86839bb6223be19d2
Denis <krollik1000000@gmail.com>
2019-11-02 16:30:00 +0200

    js

---
+++ lesson6/task8/index.js
@@ -0,0 +1,9 @@
+function uniqueCount(array) {
+    let mas = [];
+    for (let i = 0; i < array.length; i++) {
+        if (mas.indexOf(array[i]) < 0) {
+            mas.push(array[i])
+        }
+    }
+    return mas;
+}
