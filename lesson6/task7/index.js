d94616f7131b92844b07b72764716a6b175b3f41
Denis <krollik1000000@gmail.com>
2019-11-02 16:03:48 +0200

    js

--- lesson6/task7/index.js
+++ lesson6/task7/index.js
@@ -1,13 +1,12 @@
 function sortAsc(array) {
-
-    for (let i = 0, endI = array.length - 1; i < endI; i++) {
-        for (let j = 0, endJ = endI - i; j < endJ; j++) {
-            if (array[j] > array[j + 1]) {
-                let swap = array[j];
-                array[j] = array[j + 1];
-                array[j + 1] = swap;
+    for (let i = 0; i < array.length - 1; i++) {
+        for (let j = 0; j < array.length - 1 - i; j++) {
+            if (array[j + 1] < array[j]) {
+                let min = array[j + 1];
+                array[j + 1] = array[j];
+                array[j] = min;
             }
         }
     }
-    return array;
+    return array
 }
