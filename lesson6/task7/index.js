0181d9b67d18cd17d2822689f016570b7c655ccb
Denis <krollik1000000@gmail.com>
2019-11-02 16:06:29 +0200

    js

--- lesson6/task7/index.js
+++ lesson6/task7/index.js
@@ -8,5 +8,22 @@ function sortAsc(array) {
             }
         }
     }
-    return array
+    return array;
+}
+
+function sortDesc(array) {
+    for (let i = 0; i < array.length - 1; i++) {
+        for (let j = 0; j < array.length - 1 - i; j++) {
+            if (array[j + 1] < array[j]) {
+                let min = array[j + 1];
+                array[j + 1] = array[j];
+                array[j] = min;
+            }
+        }
+    }
+    let mas = [];
+    for (let i = array.length - 1; i >= 0; i--) {
+        mas.push(array[i])
+    }
+    return mas;
 }
