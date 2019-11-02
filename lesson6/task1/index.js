7985bc2d04623bb56fd7359d3611aabbefba7742
Denis <krollik1000000@gmail.com>
2019-11-02 13:46:04 +0200

    js

--- lesson6/task1/index.js
+++ lesson6/task1/index.js
@@ -1,9 +1,11 @@
 function getArrayBounds(arr) {
-    if (ArrayisArray(arr)) {
-        let mas = [arr.length, arr[0], arr[length - 1]];
-        return mas;
+    if (Array.isArray(arr)) {
+        let mas = [arr.length, arr[0], arr.length - 1];
+        console.log(mas);
     } else {
         return null;
     }
 
 };
+
+getArrayBounds([1, 2, 3, 4])
