072e3bc4ef35a2dd783856473b0fdc7b7e9640e2
Denis <krollik1000000@gmail.com>
2019-11-04 16:25:32 +0200

    js

---
+++ lesson8/task3/index.js
@@ -0,0 +1,19 @@
+function pickProps(obj, arr) {
+    let obj3 = {};
+    for (let i in object(arr)) {
+        for (let j in obj) {
+            if (i === j) {
+                obj3[i] = obj[i];
+            }
+        }
+    }
+    return obj3;
+};
+
+function object(arr) {
+    let obj2 = {};
+    arr.forEach(i => {
+        obj2[i] = i;
+    });
+    return obj2;
+};
