ba995abbcee912166fabd488c6dfef28b89905e2
Denis <krollik1000000@gmail.com>
2019-11-04 11:20:05 +0200

    js

--- lesson6/task11/index.js
+++ lesson6/task11/index.js
@@ -1,10 +1,12 @@
 function checker(arr) {
-    let max = Math.max(...arr);
-    let min = Math.min(...arr);
-
-    if (max + min > 1000) {
-        return true;
-    } else {
-        return false;
-    }
+    let max = arr[0];
+    let min = max;
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > max) {
+            max = arr[i]
+        } else if (arr[i] < max) {
+            min = arr[i];
+        }
+    };
+    return max + min
 };
