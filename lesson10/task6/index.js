56a9844d7dd796b1c9ad066d95a88d3f7fd7e1a1
Denis <krollik1000000@gmail.com>
2019-11-05 20:47:28 +0200

    js

--- lesson10/task6/index.js
+++ lesson10/task6/index.js
@@ -2,13 +2,9 @@ function getRandomNumbers(length, min, max) {
     const mass = [];
     max = Math.trunc(max);
     min = Math.trunc(min)
-    if (max < min) {
+    if (max < min || (max - min) < 1) {
         return null;
     }
-    if (!Number.isInteger(max - min)) {
-        return null;
-    }
-
     for (let i = 0; i < length; i++) {
         mass.push(Math.round(min + Math.random() * (max - min)));
     }
