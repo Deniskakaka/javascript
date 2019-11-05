e933d7acbd666f269da64f7724c6f8f09532b859
Denis <krollik1000000@gmail.com>
2019-11-05 15:37:42 +0200

    js

--- lesson9/task4/index.js
+++ lesson9/task4/index.js
@@ -1,5 +1,5 @@
 function getPeople(obj) {
     const arr = Object.values(obj);
     let mas = arr.map(i => i.map(j => { return j.name })).reduce((acc, i) => { return acc.concat(i); }, []);
-    return ьas;
+    return mas;
 };
