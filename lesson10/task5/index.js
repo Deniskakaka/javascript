4142c1b2a4a273f080f11da9cfe71cbcc7634817
Denis <krollik1000000@gmail.com>
2019-11-06 12:17:54 +0200

    js

--- lesson10/task5/index.js
+++ lesson10/task5/index.js
@@ -1,6 +1,8 @@
 function getTotalPrice(arr) {
     let count = arr.reduce((acc, number) => {
-        return acc + number;
-    });
+        return acc + Number(number.toFixed(2));
+    }, 0);
     return '$' + Number(count.toFixed(2));
 };
+
+console.log(getTotalPrice([1.123, 1.6754, 1.9943, 5.333]));
