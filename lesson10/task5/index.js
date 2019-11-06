6c2b753ed5c2ac7ac0affb371d14333c9174b224
Denis <krollik1000000@gmail.com>
2019-11-06 13:56:28 +0200

    js

--- lesson10/task5/index.js
+++ lesson10/task5/index.js
@@ -1,7 +1,7 @@
 function getTotalPrice(arr) {
     let sum = 0;
     sum = arr.reduce((acc, number) => {
-        return acc += num;
+        return acc += number;
     });
     return '$' + Math.floor(sum * 100) / 100;
 };
