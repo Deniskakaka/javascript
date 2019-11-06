88766824ad5032bf13650ccca5dc76c785509d57
Denis <krollik1000000@gmail.com>
2019-11-06 12:36:19 +0200

    js

--- lesson10/task5/index.js
+++ lesson10/task5/index.js
@@ -1,10 +1,12 @@
 function getTotalPrice(arr) {
     let sum = 0;
-    arr.forEach(i => {
-        sum += Number(i.toFixed(2));
+    arr = arr.map(i => {
+        return Number(i.toFixed(2));
     });
-    let string = '$' + Math.round(sum).toFixed(2);
-    return string;
+    sum = arr.reduce((acc, number) => {
+        return acc += number;
+    });
+    return '$' + sum.toFixed(2);
 };
 
 console.log(getTotalPrice([1.123, 1.6754, 1.9943, 5.333]));
