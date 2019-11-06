37336c15e9784411b9d59e764c204580c6b963fb
Denis <krollik1000000@gmail.com>
2019-11-06 13:33:48 +0200

    js

--- lesson10/task6/index.js
+++ lesson10/task6/index.js
@@ -14,6 +14,6 @@ function getRandomNumbers(length, min, max) {
 
 function getRandomIntInclusive(min, max) {
     min = Math.ceil(min);
-    max = Math.floor(max);
+    max = Math.ceil(max);
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
