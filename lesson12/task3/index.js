9ad6ff4f6ae9338be6367644064d4ef0e1bac81e
Denis <krollik1000000@gmail.com>
2019-11-07 14:53:27 +0200

    js

--- lesson12/task3/index.js
+++ lesson12/task3/index.js
@@ -1,10 +1,5 @@
-function cleanTransactionsList(arr) {
-    arr = arr.filter((i, index) => {
-        return !isNaN(i)
-    }).map(i => {
-        return '$' + Number(i).toFixed(2)
-    });
-    return arr;
+const cleanTransactionsList = arr => {
+    return arr.filter(i => !isNaN(i)).map(i => '$' + Number(i).toFixed(2));
 };
 
 console.log(cleanTransactionsList([' 1.9 ', '16.4', 17, ' 1 dollar ', 'sdfsdgs', '12.56', 'dfgfdgfsd', 13.45]));
