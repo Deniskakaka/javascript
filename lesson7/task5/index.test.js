64ba195763f73f7558b697d3c3aa5dbc1de2439c
Denis <krollik1000000@gmail.com>
2019-11-10 13:02:20 +0200

    js

--- lesson7/task5/index.test.js
+++ lesson7/task5/index.test.js
@@ -9,8 +9,3 @@ it('should return array in square', () => {
     let wrong = squareArray('sdfsfsdf' || 12 || true || function() {});
     expect(wrong).toEqual(null);
 });
-
-it('should return array in square', () => {
-    const nan = squareArray(['dfads', 'wdasasd', 'sfdasd'])
-    expect(nan).toEqual([NaN, NaN, NaN])
-});
