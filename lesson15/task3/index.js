f12b880e3eee7e61ac814e1499ffa7400c07ca9d
Denis <krollik1000000@gmail.com>
2019-11-11 16:26:10 +0200

    js

---
+++ lesson15/task3/index.js
@@ -0,0 +1,51 @@
+export function creatLogger() {
+    let logger = [];
+
+    function warn(str) {
+        logger.push({
+            message: str,
+            dateTime: new Date(),
+            type: 'warn'
+        })
+    };
+
+    function error(str) {
+        logger.push({
+            message: str,
+            dateTime: new Date(),
+            type: 'error'
+        });
+    };
+
+    function log(str) {
+        logger.push({
+            message: str,
+            dateTime: new Date(),
+            type: 'log'
+        })
+    };
+
+
+    function getRecords(str) {
+        let result = [];
+        result = logger.filter(i => {
+            if (i.type === str) {
+                return i;
+            }
+        });
+        if (!str) {
+            return logger;
+        } else {
+            return result.sort((a, b) => {
+                a.dateTime.getTime() - b.dateTime.getTime()
+            });
+
+        }
+    };
+    return {
+        warn,
+        error,
+        log,
+        getRecords
+    };
+};
