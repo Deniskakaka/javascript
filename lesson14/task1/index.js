30368e458035becd8aa1b949d351675efa779e49
Denis <krollik1000000@gmail.com>
2019-11-11 13:04:38 +0200

    js

--- lesson14/task1/index.js
+++ lesson14/task1/index.js
@@ -1,9 +1,9 @@
 'use strick';
 
 let message = 'Just learn it!';
-
+let who = 'Gromcode';
 export function sendMessage(name) {
-    console.log(`${name}, ${message} You ${who}`);
+    console.log(`${name}, ${message} Your ${who}`);
 };
 
 export function setMessage(text) {
