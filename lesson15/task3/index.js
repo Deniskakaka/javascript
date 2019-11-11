ff44e944bfdb1e82542a19aeaf76ac1640905a0f
Denis <krollik1000000@gmail.com>
2019-11-11 17:49:07 +0200

    js

--- lesson15/task3/index.js
+++ lesson15/task3/index.js
@@ -1,51 +1,39 @@
 export function createLogger() {
     let logger = [];
+    return {
+        warm: function(str) {
+            logger.push({
+                message: str,
+                dateTime: new Date(),
+                type: 'warm'
+            });
+        },
+        error: function(str) {
+            logger.push({
+                message: str,
+                dateTime: new Date(),
+                type: 'error'
+            });
+        },
+        log: function(str) {
+            logger.push({
+                message: str,
+                dateTime: new Date(),
+                type: 'log'
+            })
+        },
 
-    function warm(str) {
-        logger.push({
-            message: str,
-            dateTime: new Date(),
-            type: 'warn'
-        })
-    };
-
-    function error(str) {
-        logger.push({
-            message: str,
-            dateTime: new Date(),
-            type: 'error'
-        });
-    };
-
-    function log(str) {
-        logger.push({
-            message: str,
-            dateTime: new Date(),
-            type: 'log'
-        })
-    };
-
-
-    function getRecords(str) {
-        let result = [];
-        result = logger.filter(i => {
-            if (i.type === str) {
-                return i;
-            }
-        });
-        if (!str) {
-            return logger;
-        } else {
-            return result.sort((a, b) => {
+        getRecords: function(str) {
+            if (!str) {
+                return logger;
+            };
+            return logger.filter(i => {
+                if (i.type === str) {
+                    return i;
+                }
+            }).sort((a, b) => {
                 a.dateTime.getTime() - b.dateTime.getTime()
             });
-
         }
-    };
-    return {
-        warn,
-        error,
-        log,
-        getRecords
     };
 };
--- lesson15/task3/index.test.js
+++ lesson15/task3/index.test.js
@@ -1,29 +1,29 @@
-import { creatLogger } from './index.js'
+import { createLogger } from './index.js'
 
 it('should return array objects or empty array', () => {
-    let logger = creatLogger();
-    logger.warn('q');
-    let result = logger.getRecords('warn')[0].type;
-    expect(result).toEqual('warn');
+    let logger = createLogger();
+    logger.warm('q');
+    let result = logger.getRecords('warm')[0].type;
+    expect(result).toEqual('warm');
 });
 
 it('should return array objects or empty array', () => {
-    let logger = creatLogger();
-    logger.warn('q');
-    let result = logger.getRecords('warn')[0].message;
+    let logger = createLogger();
+    logger.warm('q');
+    let result = logger.getRecords('warm')[0].message;
     expect(result).toEqual('q');
 });
 
 it('should return array objects or empty array', () => {
-    let logger = creatLogger();
-    logger.warn('q');
+    let logger = createLogger();
+    logger.warm('q');
     let result = logger.getRecords('log');
     expect(result).toEqual([]);
 });
 
 it('should return array objects or empty array', () => {
-    let logger = creatLogger();
-    logger.warn('q');
+    let logger = createLogger();
+    logger.warm('q');
     logger.log('l');
     let result = logger.getRecords('error');
     expect(result).toEqual([]);
