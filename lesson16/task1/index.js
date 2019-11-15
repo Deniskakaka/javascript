5575dc57364846de9b6264c8c988fce54ac33dcd
Denis <krollik1000000@gmail.com>
2019-11-15 13:26:07 +0200

    js

--- lesson16/task1/index.js
+++ lesson16/task1/index.js
@@ -1,5 +1,11 @@
 let arr = [];
 export const createArrayOfFunctions = (number) => {
+    if (typeof number !== 'number') {
+        return null;
+    }
+    if (number === undefined) {
+        return arr;
+    }
     for (let i = 0; i < number; i++) {
         arr[i] = function() {
             return i
