de70329b19ad46c62dda0ef05a4887f2a21caa75
Denis <krollik1000000@gmail.com>
2019-11-25 13:43:49 +0200

    get and set

--- lesson17/task4/index.js
+++ lesson17/task4/index.js
@@ -1,11 +1,12 @@
 export const user = {
     firstName: "",
     lastName: "",
-    setFullName(a, b) {
-        this.firstName = a
-        this.lastName = b
-    },
     getFullName() {
         return `${this.firstName} ${this.lastName}`;
-    }
+    },
+    setFullName(fullName) {
+        let arr = fullName.split(' ');
+        this.firstName = arr[0];
+        this.lastName = arr[1];
+    },
 };
