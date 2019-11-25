412916d22401a11b03a840d85e9cc892b4c138c0
Denis <krollik1000000@gmail.com>
2019-11-25 13:07:11 +0200

    creat object

--- lesson17/task3/index.js
+++ lesson17/task3/index.js
@@ -1,3 +1,3 @@
-export const defer = (func, ms) => {
-    return function() { setTimeout(() => func.call(this, ...args), ms); }
+export function defer(func, ms) {
+    return function() { setTimeout(() => func.apply(this, args), ms); }
 };
