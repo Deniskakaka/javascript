05962d26303f8a592b2d2a9876f12f9286824dde
Denis <krollik1000000@gmail.com>
2019-11-25 23:12:47 +0200

    spy

--- lesson18/task4/index.js
+++ lesson18/task4/index.js
@@ -6,7 +6,7 @@ export function spy(func) {
 
     function wrapper(...args) {
         wrapper.calls.push(args);
-        return func.apply(null, args);
+        return func.apply(this, args);
     }
 
     wrapper.calls = [];
