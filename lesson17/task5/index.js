1756c6532489e4b048315aae9ee4e79517665190
Denis <krollik1000000@gmail.com>
2019-11-25 15:23:31 +0200

    time

--- lesson17/task5/index.js
+++ lesson17/task5/index.js
@@ -1,22 +1,22 @@
 let timerId = 0;
 export const timer = {
-    secondPassed: 0,
+    secondsPassed: 0,
     minsPassed: 0,
     startTimer() {
         timerId = setInterval(() => {
-            this.secondPassed = this.secondPassed + 5;
-            if (this.secondPassed === 60) {
+            this.secondsPassed = this.secondPassed + 5;
+            if (this.secondsPassed === 60) {
                 this.minsPassed++;
-                this.secondPassed = 0;
+                this.secondsPassed = 0;
             }
         }, 5000);
     },
     getTime() {
         if (this.secondPassed < 10) {
-            console.log(`${ + this.minsPassed}:${'0' + this.secondPassed}`)
+            console.log(`${this.minsPassed}:${'0' + this.secondsPassed}`)
         }
         if (this.secondPassed >= 10) {
-            console.log(`${ + this.minsPassed}:${+ this.secondPassed}`)
+            console.log(`${this.minsPassed}:${this.secondsPassed}`)
         }
     },
     stopTimer() {
