c6302e16d8faada0cf9232414437c9490d01ce24
Denis <krollik1000000@gmail.com>
2019-11-25 13:37:17 +0200

    set and get

---
+++ lesson17/task4/index.js
@@ -0,0 +1,14 @@
+export const user = {
+    firstName: "",
+    lastName: "",
+    setFullName(a, b) {
+        this.firstName = a
+        this.lastName = b
+    },
+    getFullName() {
+        return `${this.firstName} ${this.lastName}`;
+    }
+};
+
+user.setFullName('Denis', 'Skab');
+user.getFullName();
---
+++ lesson17/task4/index.test.js
@@ -0,0 +1,7 @@
+import { user } from './index.js'
+
+it('should set fullname and get fullname', () => {
+    let set = user.setFullName('Denis', 'Skab');
+    let result = user.getFullName();
+    expect(result).toEqual('Denis Skab');
+});
