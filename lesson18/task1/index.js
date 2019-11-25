4cf49972a197b27402cd627bd2e3f72daad5abf3
Denis <krollik1000000@gmail.com>
2019-11-25 17:51:50 +0200

    party

--- lesson18/task1/index.js
+++ lesson18/task1/index.js
@@ -1,16 +1,16 @@
- const event = {
-     message: 'Welcome to the party!',
-     guests: [
-         { name: 'John', age: 18, email: 'example@server.com' },
-         { name: 'Tom', age: 16, email: 'exa@server.com' }
-     ],
-     getInvitations() {
-         return this.guests.filter(i => i.age >= 18).map(i => {
-             return {
-                 email: i.email,
-                 message: 'Dare ' + `${i.name}` + '! ' + `${this.message}`
-             }
-         })
-     }
- }
- console.log(event.getInvitations())
+export const event = {
+    message: 'Welcome to the party!',
+    guests: [
+        { name: 'John', age: 18, email: 'example@server.com' },
+        { name: 'Tom', age: 16, email: 'exa@server.com' }
+    ],
+    getInvitations() {
+        return this.guests.filter(i => i.age >= 18).map(i => {
+            return {
+                email: i.email,
+                message: 'Dare ' + `${i.name}` + '! ' + `${this.message}`
+            }
+        })
+    }
+}
+console.log(event.getInvitations())
