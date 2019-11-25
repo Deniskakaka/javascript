c5df1ea39e68aa015d260515256633d495c80993
Denis <krollik1000000@gmail.com>
2019-11-25 21:41:45 +0200

    sum

--- lesson18/task3/index.js
+++ lesson18/task3/index.js
@@ -1,4 +1,4 @@
 export const sumOfSquares = function() {
     let arr = [...arguments];
-    let mas = arr.map(i => i * i).reduce((acc, i) => acc + i);
+    return arr.map(i => i * i).reduce((acc, i) => acc + i);
 };
