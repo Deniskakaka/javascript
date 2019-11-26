d8f8a96b35f3ba27ebb44e6f866e96505af62d9c
Denis <krollik1000000@gmail.com>
2019-11-26 13:32:15 +0200

    proto

---
+++ lesson19/task1/index.js
@@ -0,0 +1,19 @@
+export const vehicle = {
+    name: 'Argo',
+    move() {
+        console.log(`${this.name} is moving`);
+    },
+    stop() {
+        console.log(`${this.name} stopped`);
+    },
+};
+
+export const ship = {
+    startMachine() {
+        console.log(`${this.name} lifting anchor up`);
+    },
+    stopMachine() {
+        console.log(`${this.name} lifting anchor down`);
+    },
+    __proto__: vehicle
+};
