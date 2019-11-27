d33403207a9e53758b4a0b007cf2b30c93be0dd0
Denis <krollik1000000@gmail.com>
2019-11-27 11:36:14 +0200

    user

--- lesson20/task5/index.js
+++ lesson20/task5/index.js
@@ -5,3 +5,25 @@ class User {
         this._sessionld = sessionld;
     }
 };
+
+class UserRepository{
+    constructor (arr) {
+        this._users =  Object.freeze(arr);
+    }
+
+    getUserNames() {
+       let masName = [];
+       masName.push(this._users.map(i => i._name));
+       return masName
+    }
+
+    getUselds() {
+       let masId = [];
+       masId.push(this._users.map(i => i._id));
+       return masId;
+    }
+
+    getUserNameById(id) {
+       return this._users.filter(i => i._id === id);
+    }
+};
