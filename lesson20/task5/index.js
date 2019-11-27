13fe42a31de374e1e51c24a302add39f54ba7b93
Denis <krollik1000000@gmail.com>
2019-11-27 12:16:42 +0200

    user

--- lesson20/task5/index.js
+++ lesson20/task5/index.js
@@ -1,4 +1,4 @@
-export class User {
+class User {
     constructor(id, name, sessionld) {
         this._id = id;
         this._name = name;
@@ -6,7 +6,7 @@ export class User {
     }
 };
 
-export class UserRepository{
+class UserRepository{
     constructor (arr) {
         this._users =  Object.freeze(arr);
     }
@@ -21,8 +21,23 @@ export class UserRepository{
 
     getUserNameById(id) {
         let obj = this._users.filter(i => i._id === id);
+        if (obj.length < 1) {
+            return obj;
+        }
         return obj[0]._name
     }
 };
 
 
+const arr = [
+    new User(1, 'aaa', 10001),
+    new User(2, 'bbb', 10002),
+    new User(3, 'ccc', 10003),
+    new User(4, 'ddd', 10004),
+    new User(5, 'eee', 10005),
+];
+    
+ const Users = new UserRepository(arr);
+
+
+console.log(Users.getUserNameById())
