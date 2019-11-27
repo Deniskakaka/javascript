a262289ac9df36eb92b08d3f01e183366b218554
Denis <krollik1000000@gmail.com>
2019-11-27 10:30:03 +0200

    order

--- lesson20/task4/index.js
+++ lesson20/task4/index.js
@@ -1,27 +1,27 @@
 export class Order {
-
     constructor(price, city, type) {
-        this.id = `${Math.random()}`;
+        this.id = `${Math.floor(1 + Math.random() * (100 + 1 - 1))}`;
+        this.price = price;
         this.dateCreated = new Date();
         this.isConfirmed = false;
         this.dateConfirmed = null;
-        this.price = price;
         this.city = city;
         this.type = type;
     }
 
     checkPrice() {
-        if(this.price < 1000) return false;
-        return true;
+        return this.price < 1000 ? false : true;
     }
 
     confirmOrder() {
-        this.isConfirmed = true;
         this.dateConfirmed = new Date();
+        this.isConfirmed = true;
     }
 
-    isValidType() {
-        if(this.type === 'Buy' || this.type === 'Sell') return true;
+    isVaildType() {
+        if (this.type === "Buy" || this.type === "Sell") {
+            return true;
+        }
         return false;
     }
 };
