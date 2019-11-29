03804b3dd1daa8708225374ad0aa262815d87f06
Denis <krollik1000000@gmail.com>
2019-11-29 12:13:55 +0200

    to do

---
+++ lesson22/task2/index.js
@@ -0,0 +1,59 @@
+const creat = document.querySelector(".create-task-btn");
+
+let tasks =   [
+    { text: 'Buy milk', done: false },
+    { text: 'Pick up Tom from airport', done: false },
+    { text: 'Visit party', done: false },
+    { text: 'Visit doctor', done: true },
+    { text: 'Buy meat', done: true },
+];
+
+const creatObj = () => {
+    let inputText = document.querySelector(".task-input").value;
+    class task {
+        constructor (text) {
+            this.text = text,
+            this.done = false
+        }
+    };
+    let newTask = new task(inputText);
+    tasks.push(newTask);
+    render(tasks)
+}
+
+creat.addEventListener('click', creatObj);
+
+function render (arr) {
+    const list = document.querySelector('.list');
+
+    let listItems = arr.sort((a,b) => a.done - b.done).map(({text,done}) => {
+        const listItemElem = document.createElement('li');
+        const checkBox = document.createElement('input');
+        checkBox.setAttribute('type','checkbox');
+        checkBox.classList.add('list__item-checkbox');
+        checkBox.checked = done;
+        listItemElem.classList.add('list__item');
+        listItemElem.append(checkBox,text);
+        if (done) {
+            listItemElem.classList.add('done')
+        }
+        return listItemElem;
+    });
+    list.innerHTML = null;
+    return list.append(...listItems);
+};
+
+render(tasks);
+
+document.querySelector('.list').addEventListener('click',function (event) {
+    if (event.target.className === 'list__item-checkbox') {
+        event.target.parentElement.classList.toggle('done');
+        let elements = document.querySelectorAll(".list__item");
+        for (let i = 0; i < elements.length; i++) {
+           if (elements[i].className === 'list__item done') {
+               document.querySelector('.list').append(elements[i])
+           }
+        }
+    }
+});
+
