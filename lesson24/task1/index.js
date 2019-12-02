c7e3c882d8727c57b080d1d8f38b508c3f887d91
Denis <krollik1000000@gmail.com>
2019-12-02 14:15:17 +0200

    daysOfWeek

--- lesson24/task1/index.js
+++ lesson24/task1/index.js
@@ -1,4 +1,4 @@
-const daysOnWeek = ['Mo','Tu','We','Th','Fr','St','Su'];
+const daysOnWeek = ['Su','Mo','Tu','We','Th','Fr','St'];
 export function dayOfWeek (date,days) {
    const day = new Date(date).getDate();
    const newDay = new Date(date).setDate(day + days);
