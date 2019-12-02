408efce4c3fe151d2ee9f74ab4c4b49e9f761919
Denis <krollik1000000@gmail.com>
2019-12-02 13:28:59 +0200

    DateGreen

---
+++ lesson24/task2/index.js
@@ -0,0 +1,12 @@
+export const formatter = new Intl.DateTimeFormat('en', {
+    timeZone: 'UTC', 
+    hour: '2-digit',
+    minute: '2-digit',
+    hour12: false,
+ });
+ 
+ function getGreenwichTime(date) {
+     return formatter.format(date);
+ }
+ 
+ console.log(getGreenwichTime(new Date()));
