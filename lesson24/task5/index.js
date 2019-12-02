223ebe80f65b33728acd6296e8ca382815c32feb
Denis <krollik1000000@gmail.com>
2019-12-02 23:25:01 +0200

    to do

--- lesson24/task5/index.js
+++ lesson24/task5/index.js
@@ -1,13 +1,14 @@
 const creat = document.querySelector(".create-task-btn");
 
-let tasks =   [];
+let tasks =  [];
 
 
 const creatObj = () => {
     const inputText = document.querySelector(".task-input").value;
     const obj =  {
         text: inputText,
-        done: false
+        done: false,
+        dataCreate: new Date()
     };
     tasks.unshift(obj);
     render(tasks);
@@ -41,7 +42,7 @@ document.querySelector('.list').addEventListener('click',function (event) {
     if (event.target.className === 'list__item-checkbox') {
         event.target.parentElement.classList.toggle('done');
         const stringText = event.target.parentElement.textContent;
-        tasks.map((elem) => {
+        tasks.sort((a,b) => b.dataCreate - a.dataCreate).map((elem) => {
             if (elem.text === stringText) {
                 elem.done === false ? elem.done = true : elem.done = false;
             }
