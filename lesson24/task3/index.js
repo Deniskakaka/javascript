4f604ea49f400e56ad314d6a29e06d8b2fd3f11b
Denis <krollik1000000@gmail.com>
2019-12-02 16:17:01 +0200

    time

--- lesson24/task3/index.js
+++ lesson24/task3/index.js
@@ -1,10 +1,18 @@
 
 export function getDiff(startDate,endDate) {
-   let mounth = Math.abs(endDate.getMonth() - startDate.getMonth() )* 30;
-   let yearInDays = Math.abs(endDate.getFullYear() - startDate.getFullYear()) * 365;
-   let days =  mounth + (Math.abs(endDate.getDate() - startDate.getDate())) + yearInDays;
-   let hour = Math.abs(endDate.getHours() - startDate.getHours());
-   let minut = Math.abs(endDate.getMinutes() - startDate.getMinutes());
-   let second = Math.abs(endDate.getSeconds() - startDate.getSeconds())
-   return `${days}d ${hour}h ${minut}m ${second}s`;
+   let DiffTime = startDate - endDate;
+   let second = (parseInt(DiffTime / 1000));
+   let days = Math.trunc(second / (24 * 60 * 60));
+   let secondInLastDay = second - days * 24 * 3600;
+   let hour = (parseInt(secondInLastDay / 3600));
+   let secInLastHour = secondInLastDay - hour * 3600;
+   let minut = (parseInt(secInLastHour / 60));
+   let sec = secInLastHour - minut * 60;
+   return `${days}d ${hour}h ${minut}m ${sec}s`
 };
+
+
+const startDate = new Date(Date.UTC(2019, 10, 24, 20, 50, 34));
+const endDate = new Date(Date.UTC(2019, 5, 22, 17, 52, 18));
+
+console.log(getDiff(startDate,endDate));
