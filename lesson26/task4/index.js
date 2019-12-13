3b4ed4262452ffaec0b71757bcfcdee224e90066
Denis <krollik1000000@gmail.com>
2019-12-13 18:13:20 +0200

    time

--- lesson26/task4/index.js
+++ lesson26/task4/index.js
@@ -1,26 +1,33 @@
-export function shmoment (value) {
+ function shmoment (value) {
     let result;
+
+    const getMethodAdd =  {
+        years: function(val){ return value.setFullYear(value.getFullYear() + val)},
+        month: function(val){ return value.setMonth(value.getMonth() + val)},
+        days: function(val){ return value.setDate(value.getDate() + val)},
+        hours: function(val){ return value.setHours(value.getHours() + val)},
+        minutes: function(val){ return  value.setMinutes(value.getMinutes() + val)},
+        second: function(val){ return value.setSeconds(value.getSeconds() + val)},
+        milliseconds: function(val){ return value.setMilliseconds(value.getMilliseconds() + val)},
+    }
+    const getMethodSub =  {
+        years: function(val){ return value.setFullYear(value.getFullYear() - val)},
+        month: function(val){ return value.setMonth(value.getMonth() - val)},
+        days: function(val){ return value.setDate(value.getDate() - val)},
+        hours: function(val){ return value.setHours(value.getHours() - val)},
+        minutes: function(val){ return  value.setMinutes(value.getMinutes() - val)},
+        second: function(val){ return value.setSeconds(value.getSeconds() - val)},
+        milliseconds: function(val){ return value.setMilliseconds(value.getMilliseconds() + val)},
+    }
 
     const time = {
         add(str,val) {
-            if (str === 'minutes') result = value.setMinutes(value.getMinutes() + val);
-            if (str === 'seconds') result = value.setSeconds(value.getSeconds() + val);
-            if (str === 'milliseconds') result = value.setMilliseconds(value.getMilliseconds() + val);
-            if (str === 'hours') result = value.setHours(value.getHours() + val);
-            if (str === 'days') result = value.setDate(value.getDate() + val);
-            if (str === 'months') result = value.setMonth(value.getMonth() + val);
-            if (str === 'years') result = value.setFullYear(value.getFullYear() + val);
+            result = getMethodAdd[str](val);
             return this
         },
         subtract(str,val) {
-            if (str === 'minutes') result = value.setMinutes(value.getMinutes() - val);
-            if (str === 'seconds') result = value.setSeconds(value.getSeconds() - val);
-            if (str === 'milliseconds') result = value.setMilliseconds(value.getMilliseconds() - val);
-            if (str === 'hours') result = value.setHours(value.getHours() - val);
-            if (str === 'days') result = value.setDate(value.getDate() - val);
-            if (str === 'months') result = value.setMonth(value.getMonth() - val);
-            if (str === 'years') result = value.setFullYear(value.getFullYear() - val);
-            return this
+            result = getMethodSub[str](val);
+            return this;
         },
         result() {
             return new Date(result);
@@ -29,5 +36,6 @@ export function shmoment (value) {
     return time;
 };
 
+console.log(shmoment(new Date(2020, 0, 7, 17, 17, 17)).add('days', 2).subtract('years',10).result())
 
 
