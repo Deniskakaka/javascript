519cfae0d540342dd71e8e0f1fefe301385f154f
Denis <krollik1000000@gmail.com>
2019-12-13 15:08:08 +0200

    favorite

--- lesson26/task2/index.js
+++ lesson26/task2/index.js
@@ -7,18 +7,3 @@ export const markFavorites = (tree,favorites) => {
         nodes: tree.nodes.map(childNode => markFavorites(childNode,favorites))
     }
 }
-
-const favorites = ['id-2'];
-const tree = {
-    id: 'id-1',
-    name: 'Products',
-    nodes: [
-        {
-            id: 'id-2',
-            name: 'Food',
-            nodes: []
-        },
-    ],
-};
-
-console.log(markFavorites(tree,favorites));
