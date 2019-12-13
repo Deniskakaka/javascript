8fdfce3ca3d9fab4cf366fa604db65444705212e
Denis <krollik1000000@gmail.com>
2019-12-13 15:06:19 +0200

    favorite

---
+++ lesson26/task2/index.js
@@ -0,0 +1,24 @@
+const markFavorites = (tree,favorites) => {
+    const isFavorite = favorites.include(tree.id);
+    
+    return {
+        ...tree,
+        isFavorite,
+        nodes: tree.nodes.map(childNode => markFavorites(childNode,favorites))
+    }
+}
+
+const favorites = ['id-2'];
+const tree = {
+    id: 'id-1',
+    name: 'Products',
+    nodes: [
+        {
+            id: 'id-2',
+            name: 'Food',
+            nodes: []
+        },
+    ],
+};
+
+console.log(markFavorites(tree,favorites));
