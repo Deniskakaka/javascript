e3fc22f03120663272720144183224730fcce2fd
Denis <krollik1000000@gmail.com>
2019-12-15 15:46:00 +0200

    fib

--- lesson26/task5/index.js
+++ lesson26/task5/index.js
@@ -1,14 +1,12 @@
-export function maxFibonacci(value) {
-  let fibonacci = [0, 1];
-  for (i = 2; i < value; i ++) {
-    fibonacci[i] = fibonacci[i-1] + fibonacci[i-2];
-  }
-  if (value === 1) {
-    return value
-  }
-  for (let i = 0; i < fibonacci.length; i++) {
-    if (fibonacci[i] >= value) {
-      return fibonacci[i - 1]
-    }
-  }
-}
+export function maxFibonacci(n) {
+  let a = 1, b = 1;
+  for (let i = 3; i <= n; i++) {
+    let c = a + b;
+    a = b;
+    b = c;
+    if (b >= n) return a
+  }         
+ return b;
+ }
+
+console.log(maxFibonacci(125))
