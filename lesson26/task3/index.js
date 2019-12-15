46a48a80f5f23438c9b17e50bafbb39b9f266947
Denis <krollik1000000@gmail.com>
2019-12-15 11:45:43 +0200

    car

--- lesson26/task3/index.js
+++ lesson26/task3/index.js
@@ -1,9 +1,9 @@
 export const mult = a => b => a * b;
 
-export function twice(a) {
-    return mult(2)(a);
+export function twice(b) {
+    return mult(2)(b);
 };
 
-export function triple(a){
-    return mult(3)(a);
+export function triple(b){
+    return mult(3)(b);
 }
