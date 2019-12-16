51b603b43a00fd6b42fe91383a6fc93be43de35d
Denis <krollik1000000@gmail.com>
2019-12-16 18:00:52 +0200

    size

--- lesson27/task1/index.js
+++ lesson27/task1/index.js
@@ -7,7 +7,7 @@ export const addImage = (url,callback) => {
 
     const onImageLoaded = () => {
         const {width, height} = imgElem;
-        callback(null, {width: 200, height: 100});
+        callback(null, {width, height});
     };
 
     imgElem.addEventListener('load', onImageLoaded);
