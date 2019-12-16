aaf0691408608791020f0f6d5e8131c01a7cf087
Denis <krollik1000000@gmail.com>
2019-12-16 17:25:05 +0200

    funct

--- lesson27/task4/index.js
+++ lesson27/task4/index.js
@@ -2,10 +2,10 @@ export const requestUserData = (userId, callback) =>{
     let random = Math.floor(getRandomArbitrary(1, 4));
         setTimeout(() => {
             if (userId === 'broken') {
-                callback(null,'Failed to load user data');
+                 callback(null,'Failed to load user data');
                  return;
-            } else {
-              return callback({name: 'John', age: 17, userId: 'userid777', email: 'userid777@example.com'});
+            } else if (userId === 'userid777') {
+               callback({name: 'John', age: 17, userId: 'userid777', email: 'userid777@example.com'});
             }    
         }, random * 1000);
 }
