5e71cfd2cc782454025ed7c81c06dfb17d36f161
Denis <krollik1000000@gmail.com>
2019-12-16 12:37:32 +0200

    fib

--- lesson26/task5/index.js
+++ lesson26/task5/index.js
@@ -1,15 +1,17 @@
 export function maxFibonacci(n) {
   let a = 0, b = 1;
-  if(n <= 1) {
+  if(n < 1) {
     return 0;
   }
-  for (let i = 2; i <= n; i++) {
-    let c = a + b;
-    a = b;
-    b = c;
-    if (b >= n) return a
+  for (let i = 0; i <= n; i++) {
+    if (a > n) {
+      return b - a;
+    } else {
+      b = a + b;
+      a = b - a;
+    }
   }         
-  return b;
-}
+  return a;
+};
 
-console.log(maxFibonacci(15));
+console.log(maxFibonacci(5.7))
--- lesson26/task5/index.test.js
+++ lesson26/task5/index.test.js
@@ -1,8 +1,8 @@
 import {maxFibonacci} from './index.js'
 
 it('should return number fibanacci before value in function', () => {
-    let result = maxFibonacci(5.7);
-    expect(result).toEqual(5);
+    let result = maxFibonacci(8);
+    expect(result).toEqual(8);
 });
 
 it('should return number fibanacci before value in function', () => {
@@ -17,10 +17,15 @@ it('should return number fibanacci before value in function', () => {
 
 it('should return number fibanacci before value in function', () => {
     let result = maxFibonacci(3);
-    expect(result).toEqual(2);
+    expect(result).toEqual(3);
 });
 
 it('should return number fibanacci before value in function', () => {
     let result = maxFibonacci(15.7);
     expect(result).toEqual(13);
+});
+
+it('should return number fibanacci before value in function', () => {
+    let result = maxFibonacci(8);
+    expect(result).toEqual(8);
 });
