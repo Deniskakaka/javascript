a27b4fbef564093752756298467bf2eb5d33aa1b
Denis <krollik1000000@gmail.com>
2019-12-16 14:36:59 +0200

    callback

--- lesson27/task1/index.js
+++ lesson27/task1/index.js
@@ -2,12 +2,14 @@ export const addImage = (url,callback) => {
     const imgElem = document.createElement('img');
     imgElem.setAttribute('alt','photo');
     imgElem.src = url;
-    const container = document.querySelector('.page');
-    container.append(imgElem);
+    const containerElem = document.querySelector('.page');
+    containerElem.append(imgElem);
+
     const onImageLoaded = () => {
         const {width, height} = imgElem;
         callback(null, {width: 200, height: 100});
-    }
+    };
+
     imgElem.addEventListener('load', onImageLoaded);
 
     imgElem.addEventListener('error', () => callback('Image load failed'));
