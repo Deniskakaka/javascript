8fa344614757b02e41534ac3d891ec5cd6ab4af5
Denis <krollik1000000@gmail.com>
2019-12-16 16:05:59 +0200

    func

---
+++ lesson27/task4/index.js
@@ -0,0 +1,21 @@
+export const requestUserData = (userId, callback) =>{
+    let random = Math.floor(getRandomArbitrary(1, 4));
+    if (userId === 'broken') {
+      return  callback('Failed to load user data');
+    } else {
+        setTimeout(() => {
+            return (userId);
+        });
+    }
+}
+
+function getRandomArbitrary(min, max) {
+    return Math.random() * (max - min) + min;
+}
+
+const back = (value) => {
+    return value;
+}
+
+
+console.log(requestUserData('broken',back))
