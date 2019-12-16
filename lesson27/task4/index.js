56ad25de0c600784c01b3412b5763bfe270a03fc
Denis <krollik1000000@gmail.com>
2019-12-16 17:39:45 +0200

    func

--- lesson27/task4/index.js
+++ lesson27/task4/index.js
@@ -5,7 +5,7 @@ export const requestUserData = (userId, callback) =>{
                  callback(null,'Failed to load user data');
                  return;
             } else {
-               callback({ name: 'John', age: 17, userId: 'userid777', email: `${userId}777@example.com`});
+               callback({ name: 'John', age: 17, userId: 'userid777', email: `${userId}@example.com`});
             }    
         }, random * 1000);
 }
