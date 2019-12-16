f01d0c8c3430be49de3e12a99323f154effec0dc
Denis <krollik1000000@gmail.com>
2019-12-16 13:08:52 +0200

    fib

--- lesson26/task5/index.js
+++ lesson26/task5/index.js
@@ -1,17 +1,13 @@
 export function maxFibonacci(n) {
-  let a = 0, b = 1;
-  if(n < 1) {
+  let a = 1, b = 1;
+  if(n <= 1) {
     return 0;
   }
-  for (let i = 0; i <= n; i++) {
-    if (a > n) {
-      return b - a;
-    } else {
-      b = a + b;
-      a = b - a;
-    }
+  for (let i = 2; i <= n; i++) {
+    let c = a + b;
+    a = b;
+    b = c;
+    if (b > n) return a
   }         
-  return a;
+  return b;
 };
-
-console.log(maxFibonacci(5.7))
