a8e7223530b5102efc6faf0595eb63567fb2ceed
Denis <krollik1000000@gmail.com>
2019-12-17 20:05:45 +0200

    promise cal

--- lesson30/task1/index.js
+++ lesson30/task1/index.js
@@ -1,11 +1,27 @@
-export function asyncSum(...asyncNumbers) {
-    return Promise.all(asyncNumbers)
+function getValueWithDelay(value, delay) {
+     return new Promise(resolve => {
+         setTimeout(() => {
+             console.log(value);
+             resolve(value)
+         }, delay);
+     });
+ };
+
+const getSum = (numbers) =>
+    numbers
+        .filter(value => !isNaN(value))
+        .reduсe((acc,num) => acc + Number(num), 0);
+        
+const asyncSum = (...asyncNumber) => {
+    return Promise.all(asyncNumber)
         .then(numbers => getSum(numbers))
-        .catch(() => Promise.reject(new Error('Can\'t calculate')));
+        .catch(() => Promise.reject(new Error('Can\'t calculate')))
 };
 
-function getSum(numbers) {
-    return numbers
-        .filter(elem => !isNaN(elem))
-        .reduce((acc, elem) => acc + Number(elem), 0);
-};
+
+
+const Num1 = getValueWithDelay(2, 2000);
+const Num2 = getValueWithDelay(15, 1000);
+const Num3 = getValueWithDelay(10, 2000);        
+asyncSum(Num1, Num2, Num3)
+     .then(result => console.log(result));
