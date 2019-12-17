a4ca46fdfe0ec65f06d8bc8b7330fe6f280739a8
Denis <krollik1000000@gmail.com>
2019-12-17 13:39:08 +0200

    calculator promise

--- lesson29/task2/index.js
+++ lesson29/task2/index.js
@@ -1,16 +1,21 @@
 export const asyncCalculator = value => new Promise ((resolve) => {
     setTimeout(() => {
         console.log(`Initial value: ${value}`);
-       return  resolve(value);
+        resolve(value);
     },500);
-}).then(value => new Promise((resolve) => {
-    setTimeout(() => {
-        let result = value * value;
-        console.log(`Squared value: ${result}`);
-       return  resolve(result);
-    },500);
-})).then(value => {
-    let result = value * 2;
-    console.log(`Doubled value: ${result}`);
-});
-//asyncCalculator(5);
+})
+    .then(value => new Promise((resolve) => {
+        setTimeout(() => {
+            let result = value * value;
+            console.log(`Squared value: ${result}`);
+            resolve(result);
+        },500);
+    }))
+    .then(value => new Promise((resolve) => {
+        let result = value * 2;
+        console.log(`Doubled value: ${result}`);
+        resolve(result)
+    })
+   
+);
+asyncCalculator(5);
