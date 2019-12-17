ce733d5fe7df479ae0decc8189cf0a3e2837695c
Denis <krollik1000000@gmail.com>
2019-12-17 12:06:45 +0200

    time promise

--- lesson28/task2/index.js
+++ lesson28/task2/index.js
@@ -1,7 +1,8 @@
 export function delay(time) {
-    const p = new Promise((resolve, reject) => {
+   const p = new Promise((resolve, reject) => {
         setTimeout(() => {
             resolve(console.log(resolve));
         },time);
     });
+    return p;
 };
