9e49f5aa2fa0b53e7e8d76b67459f74ee1de16e6
Denis <krollik1000000@gmail.com>
2019-12-17 18:34:32 +0200

    promise calculator

--- lesson30/task1/index.js
+++ lesson30/task1/index.js
@@ -2,7 +2,7 @@ function getSum(numbers) {
     numbers.filter(value => !isNaN(value))
            .reduse((acc,num) => acc + num,0);
 }
-export const asyncSum = (...asyncNumber) {
+export const asyncSum = (...asyncNumber) => {
     return Promise.all(asyncNumber)
         .then(number => getSum(number))
         .catch(() => Promise.reject(new Error('Can\'t calculate')))
