318bb1fc6df62f58af40a082c62167d4adde6584
Denis <krollik1000000@gmail.com>
2019-12-17 18:48:59 +0200

    promise cal

--- lesson30/task1/index.js
+++ lesson30/task1/index.js
@@ -1,9 +1,10 @@
-function getSum(numbers) {
-  return  numbers.filter(value => !isNaN(value))
-           .reduse((acc,num) => acc + Number(num), 0);
-}
 export const asyncSum = (...asyncNumber) => {
     return Promise.all(asyncNumber)
         .then(numbers => getSum(numbers))
         .catch(() => Promise.reject(new Error('Can\'t calculate')))
-}
+};
+
+const getSum = (numbers) =>
+    numbers
+        .filter(value => !isNaN(value))
+        .reduse((acc,num) => acc + Number(num), 0);
