36b2cd8944ff2b37d6612c58ce48cc1fbad523ea
Denis <krollik1000000@gmail.com>
2019-12-18 10:22:14 +0200

    гкд ыукмук

--- lesson30/task1/index.js
+++ lesson30/task1/index.js
@@ -12,7 +12,7 @@ const getSum = (numbers) =>
         .filter(value => !isNaN(value))
         .reduce((acc,num) => acc + Number(num), 0);
         
-export const asyncSum = (...asyncNumber) => {
+const asyncSum = (...asyncNumber) => {
     return Promise.all(asyncNumber)
         .then(numbers => getSum(numbers))
         .catch(() => Promise.reject(new Error('Can\'t calculate')))
@@ -20,8 +20,8 @@ export const asyncSum = (...asyncNumber) => {
 
 
 
-const Num1 = getValueWithDelay(2, 2000);
-const Num2 = getValueWithDelay(15, 1000);
-const Num3 = getValueWithDelay(10, 2000);        
-asyncSum(Num1, Num2, Num3)
+const Num1 = getValueWithDelay('', 2000);
+const Num2 = getValueWithDelay('dfs', 1000);
+const Num3 = getValueWithDelay(undefined, 2000);        
+asyncSum(Num1, Num2, Num3, Promise.reject(new Error('wopa')))
      .then(result => console.log(result));
