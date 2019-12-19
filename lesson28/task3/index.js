0c8103e743bf0522d7365dff711dce1b43b8a29e
Denis <krollik1000000@gmail.com>
2019-12-19 16:45:38 +0200

    git

--- lesson28/task3/index.js
+++ lesson28/task3/index.js
@@ -1,6 +1,6 @@
 import{addImage} from './addImage.js'
 
-export function addImageV2(imgSrc) {
+function addImageV2(imgSrc) {
     return new Promise((resolve, reject) => {
         addImage(imgSrc, (error, data) => {
             if (error) {
@@ -12,3 +12,5 @@ export function addImageV2(imgSrc) {
     });
 };
 
+
+addImageV2('https://images.wallpaperscraft.ru/image/ruka_paltsy_zvezda_153256_1920x1080.jpg').then(result => console.log(result))
---
+++ lesson32/task2/index.js
@@ -0,0 +1,45 @@
+let avatar = document.querySelector('.user__avatar');
+let name = document.querySelector('.name');
+let search = document.querySelector('.search');
+let number = document.querySelector('.number');
+let user = document.querySelector('.userId');
+let repo = document.querySelector('.repoId');
+
+function fetchUser(userId, repoId) {
+    return fetch(`https://api.github.com/repos/${userId}/${repoId}/commits`)
+           .then(response => response.json())
+}
+
+function render(days, value) {
+    let searchDays = new Date().setDate(new Date().getDate() - days);
+    let commit = value.map(({ commit: {author : {email, date, name} } }) => ({
+        date,
+        name,
+        email,
+    })).filter(i => new Date(i.date).getDate() > +new Date(searchDays).getDate())
+    .reduce((acc, { email, name }) => {
+        const oldCount = acc[email] ? acc[email].count : 0;
+        return {
+            ...acc,
+            [email]: {  count: oldCount + 1, name, email}
+        };
+    }, {});
+    let mass = [];
+    for (let key in commit) {
+        mass.push(commit[key])
+    }
+    mass.sort((a,b) => b.count - a.count);
+    const avatarUrl = value.filter(i => i.commit.author.email === mass[0].email).map(i => i.author.avatar_url)[0]
+    avatar.src = avatarUrl;
+    name.textContent = mass[0].name;
+}
+
+function getMostActiveDevs(obj) {
+      fetchUser(obj.userId,obj.repoId).then(result => render(obj.days, result));
+}
+
+
+
+search.addEventListener('click', () => {
+    getMostActiveDevs({days: number.value, userId: user.value, repoId: repo.value})
+})
