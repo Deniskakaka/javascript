01f6192d82a5a722edcc0e6637cbabdb441dc26a
Denis <krollik1000000@gmail.com>
2019-12-20 16:35:31 +0200

    git fetch

---
+++ lesson32/task1/index.js
@@ -0,0 +1,22 @@
+const button = document.querySelector('.name-form__btn');
+const string = document.querySelector('.name-form__input');
+const avatar = document.querySelector('.user__avatar');
+const name = document.querySelector('.user__name');
+const loc = document.querySelector('.user__location');
+
+function getUser(str) {
+   return  fetch(`https://api.github.com/users/${str}`)
+    .then(response => response.json())
+    .then(result => result);
+};
+
+function render(obj) {
+    console.log(obj)
+    avatar.src = obj.avatar_url;
+    name.textContent = obj.name;
+    loc.textContent = obj.location;
+}
+
+button.addEventListener('click', () =>{
+    getUser(string.value).then(result => render(result))
+});
