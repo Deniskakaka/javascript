ab528d74b39092f5f6bf6facb4ecc83395a68646
Denis <krollik1000000@gmail.com>
2019-12-26 18:51:55 +0200

    git commit

--- lesson34/task3/index.js
+++ lesson34/task3/index.js
@@ -1,9 +1,9 @@
 import { fetchUserData, fetchRepositories } from './gateWays.js'
 import { renderUserData } from './user.js'
 import {renderRepos} from './repos.js'
+import {showSpinner, hideSpinner} from './spiner.js'
 
 const showUserBtnElem = document.querySelector('.name-form__btn');
-const spiner = document.querySelector('.spinner_hidden');
 const defaultUser = {
     avatar_url :  `https://avatars3.githubusercontent.com/u10001`,
     name: '',
@@ -13,24 +13,21 @@ const defaultUser = {
 renderUserData(defaultUser);
 
 const onSearchUser = () => {
+    showSpinner();
     const userName = document.querySelector('.name-form__input').value;
     fetchUserData(userName)
     .then(userData => {
         renderUserData(userData);
-        spiner.style.display = 'none'
         return userData.repos_url
     })
     .then(url => fetchRepositories(url))
     .then(reposList => {
-        return renderRepos(reposList);
+        renderRepos(reposList);
+        hideSpinner();
     })
     .catch(err => {
-        spiner.style.display = 'none'
         alert(err.message)
     })
 };
 
-showUserBtnElem.addEventListener('click',() => {
-    onSearchUser()
-    spiner.style.display = 'block'
-});
+showUserBtnElem.addEventListener('click',onSearchUser);
