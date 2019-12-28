019ecc45347de5a48df8f5f52d5b1fc83f510033
Denis <krollik1000000@gmail.com>
2019-12-28 16:00:19 +0200

    async

--- lesson35/task3/index.js
+++ lesson35/task3/index.js
@@ -1,4 +1,4 @@
-async function getUserBlogs(urls) {
+export async function getUserBlogs(urls) {
     let mass  = [];
     for (let i = 0; i < urls.length; i++) {
        let  response = await fetch(`https://api.github.com/users/${urls[i]}`)
