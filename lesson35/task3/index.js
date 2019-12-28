8bedc82c647cce3870bbfca0d77b3cb257aacdca
Denis <krollik1000000@gmail.com>
2019-12-28 16:12:39 +0200

    async

--- lesson35/task3/index.js
+++ lesson35/task3/index.js
@@ -1,13 +1,19 @@
-export async function getUsersBlogs(urls) {
+async function getUsersBlogs(urls) {
     let mass  = [];
     for (let i = 0; i < urls.length; i++) {
-       let  response = await fetch(`https://api.github.com/users/${urls[i]}`)
-        if (!response.ok) {
-            return null;
-        } else {
-             mass.push(await response.json())
+        try{
+            let  response = await fetch(`https://api.github.com/users/${urls[i]}`);
+            if (!response.ok) {
+                return null;
+            }
+            let result = await response.json()
+            mass.push(result.blog)  
+        } catch(err) {
+            throw new Error ('faild')
         }
-       
     }
-   return mass.map(i => i.blog)
+    return mass;
 };
+
+getUsersBlogs(['', 'facebook', 'gaearon']).then(result => console.log(result))
+
