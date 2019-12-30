11ee972c094ee2711163a477fcc7fd19686c2f8e
Denis <krollik1000000@gmail.com>
2019-12-30 12:53:26 +0200

    async

--- lesson35/task3/index.js
+++ lesson35/task3/index.js
@@ -1,10 +1,17 @@
-export async function getUsersBlogs(urls) {
+ async function getUsersBlogs(urls) {
     const requests = urls
-    .map(userId => fetch(`https://api.github.com/users/${userId}`).then(response => response.json()));
+    .map(userId => fetch(`https://api.github.com/users/${userId}`)
+    .then(response => {
+        if (response.ok) {
+           return  response.json()
+        } else {
+            throw new Error('Failed to load data')
+        }
+    }));
     const usersData = await Promise.all(requests);
     return usersData.map(user => user.blog)
 };
 
-getUsersBlogs(['', 'facebook', 'gaearon','twitter']).then(result => console.log(result))
+getUsersBlogs(['google', 'facebook', 'gaearon','twitter']).then(result => console.log(result))
     
 
